89834e7c62b60c6ec74dafafccd942268a95a0d4
Nikos S <niksal1991@gmail.com>
2024-08-08 09:02:56 +0300

    Renamed class

---
+++ Auth.Identity.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Auth.Identity.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auth.Identity.Infrastructure
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+        options.UseSqlite("DataSource = identityDb; Cache=Shared");
+    }
+}
