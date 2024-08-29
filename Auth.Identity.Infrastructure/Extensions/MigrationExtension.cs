507a4184ae2c66e19eed0443e42b2b67c1fe8c93
Nikos S <niksal1991@gmail.com>
2024-08-29 19:22:45 +0300

    Added MigrationExtension.cs

---
+++ Auth.Identity.Infrastructure/Extensions/MigrationExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Auth.Identity.Infrastructure.Extensions
+{
+    public static class MigrationExtension
+    {
+        public static void ApplyMigrations(this IApplicationBuilder applicationBuilder)
+        {
+            using IServiceScope serviceScope = applicationBuilder.ApplicationServices.CreateScope();
+            using ApplicationDbContext applicationDbContext = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            applicationDbContext.Database.Migrate();
+        }
+    }
+}
