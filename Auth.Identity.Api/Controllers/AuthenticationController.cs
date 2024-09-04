e72b7ea88f70f10a76fdb90619e34c932888d16d
Nikos S <niksal1991@gmail.com>
2024-09-04 08:32:08 +0300

    Added controller

---
+++ Auth.Identity.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Auth.Identity.Api.Controllers
+{
+    [Route("api/v1/authentication")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+    }
+}
